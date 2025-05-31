b93ae07f674ce68a2d9248449b17b8152ace65ad
Erbol Tokurov <erbol.tokur@gmail.com>
2025-06-01 05:57:43 +0600

    project reupload for ue 4.27

---
+++ Source/KyrgyzFable/KyrgyzFableGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KyrgyzFableGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KYRGYZFABLE_API AKyrgyzFableGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
