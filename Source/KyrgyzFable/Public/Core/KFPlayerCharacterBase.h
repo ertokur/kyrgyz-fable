48fd3731f12d083764bc0886e7d9abe5ad8c0e2d
Erbol Tokurov <erbol.tokur@gmail.com>
2025-06-07 15:54:31 +0600

    [feat]: Third person assets

--- Source/KyrgyzFable/Public/Core/KFPlayerCharacterBase.h
+++ Source/KyrgyzFable/Public/Core/KFPlayerCharacterBase.h
@@ -11,18 +11,35 @@ class KYRGYZFABLE_API AKFPlayerCharacterBase : public ACharacter
 {
 	GENERATED_BODY()
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+	
 public:
-	// Sets default values for this character's properties
 	AKFPlayerCharacterBase();
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	float BaseLookUpRate;
 
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	void MoveForward(float Value);
+
+	void MoveRight(float Value);
+
+	void TurnAtRate(float Rate);
+	
+	void LookUpAtRate(float Rate);
+	
 public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
