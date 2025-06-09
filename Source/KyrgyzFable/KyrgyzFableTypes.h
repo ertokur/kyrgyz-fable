2a77e49c51be51982298bac140ddd853adeb58a3
Erbol Tokurov <erbol.tokur@gmail.com>
2025-06-09 11:00:06 +0600

    [feat]: Added new structs for localization

--- Source/KyrgyzFable/KyrgyzFableTypes.h
+++ Source/KyrgyzFable/KyrgyzFableTypes.h
@@ -3,11 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "KyrgyzFableTypes.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FMissionEventSignature);
 
 DECLARE_LOG_CATEGORY_EXTERN(LogMission, Log, All);
+
+UENUM(BlueprintType)
+enum class EMissionStepState : uint8
+{
+	MSS_NotActive	UMETA(DisplayName = "Not Active"),
+	MSS_Active		UMETA(DisplayName = "Active"),
+	MSS_Completed	UMETA(DisplayName = "Completed")
+};
+
+UENUM(BlueprintType)
+enum class ELanguage : uint8
+{
+	L_Russian		UMETA(DisplayName = "Russian"),
+	L_Kyrgyz		UMETA(DisplayName = "Kyrgyz"),
+	L_English		UMETA(DisplayName = "English")
+};
 
 USTRUCT(BlueprintType)
 struct FMissionSteps
@@ -18,12 +35,40 @@ struct FMissionSteps
 	TArray<FName> MissionStepNames;
 };
 
-UENUM(BlueprintType)
-enum class EMissionStepState : uint8
+USTRUCT(BlueprintType)
+struct FLocalization
 {
-	MSS_NotActive	UMETA(DisplayName = "Not Active"),
-	MSS_Active		UMETA(DisplayName = "Active"),
-	MSS_Completed	UMETA(DisplayName = "Completed")
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Russian;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Kyrgyz;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText English;
+};
+
+USTRUCT(BlueprintType)
+struct FLocalizationTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLocalization Localization;
+};
+
+USTRUCT(BlueprintType)
+struct FDialogTableRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FLocalization Localization;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* Sound = nullptr;
 };
 
 //DELEGATES
