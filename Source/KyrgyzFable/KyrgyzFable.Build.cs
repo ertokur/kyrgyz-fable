f0bf5839251e61d13d085c293a3dff486590b8f6
Erbol Tokurov <erbol.tokur@gmail.com>
2025-06-17 03:03:58 +0600

    [feat]: Parental control

--- Source/KyrgyzFable/KyrgyzFable.Build.cs
+++ Source/KyrgyzFable/KyrgyzFable.Build.cs
@@ -8,7 +8,7 @@ public class KyrgyzFable : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "DeveloperSettings" });
 		
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
---
+++ Source/KyrgyzFable/Settings/KyrgyzFableSettings.h
@@ -0,0 +1,20 @@
+﻿// Erbol Tokur
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettings.h"
+#include "KyrgyzFableSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(config=Game, DefaultConfig)
+class KYRGYZFABLE_API UKyrgyzFableSettings : public UDeveloperSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, EditAnywhere, BlueprintReadOnly)
+	FString ParentalControlPassword = "123456";
+};
